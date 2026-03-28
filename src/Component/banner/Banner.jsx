f37a90fbb0e9024223188b5cf3d06c63d29e43ad
Safvan P <pcsajitha87@gmail.com>
2026-03-28 20:19:19 +0530

    hello

--- src/Component/banner/Banner.jsx
+++ src/Component/banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./banner.css";
+import "./Banner.css";
 import Navbar from "../Navbar/Navbar";
 import MovieList from "../../MovieList";
 import Footer from "../../Footer";
